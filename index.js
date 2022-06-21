a5cb0afe7befb6d7b766c719b62bf3ea8691225c
AzizRAA <azizbekruzmatovraa@gmail.com>
2022-06-21 13:10:09 +0300

    fixed linter errors

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const navToggler = document.querySelector(".nav-toggler");
-navToggler.addEventListener("click", navToggle);
+const navToggler = document.querySelector('.nav-toggler');
 
 function navToggle() {
-   navToggler.classList.toggle("active");
-   const nav = document.querySelector(".nav");
-   nav.classList.toggle("open");
-   if(nav.classList.contains("open")){
-       nav.style.maxHeight = nav.scrollHeight + "px";
-   }
-   else{
-       nav.removeAttribute("style");
-   }
-} 
+  navToggler.classList.toggle('active');
+  const nav = document.querySelector('.nav');
+  nav.classList.toggle('open');
+  if (nav.classList.contains('open')) {
+    nav.style.maxHeight = `${nav.scrollHeight}px`;
+  } else {
+    nav.removeAttribute('style');
+  }
+}
+
+navToggler.addEventListener('click', navToggle);
